d83d530454432cbe95a5b745bc516139cceccdb1
Viacheslav <vyacheslav.kostyanets@gmail.com>
2023-10-25 22:36:50 +0300

    Output data inside index.html template

--- main.go
+++ main.go
@@ -12,6 +12,27 @@ func main() {
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	view, _ := template.ParseFiles("index.html")
-	data := "HELLO WORLD"
-	view.Execute(w, data)
+	house := map[string]int{
+		"floors":  10,
+		"windows": 1000,
+		"rooms":   100,
+	}
+	personOne := Person{
+		Name:   "Slava",
+		Age:    30,
+		Health: true,
+	}
+
+	alldata := map[string]interface{}{
+		"House":  house,
+		"Person": personOne,
+	}
+
+	view.Execute(w, alldata)
+}
+
+type Person struct {
+	Name   string
+	Age    int
+	Health bool
 }
